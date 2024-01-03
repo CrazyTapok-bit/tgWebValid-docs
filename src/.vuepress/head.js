7ad8c6c3e94a153a33c281419d0c7de8a2b018d5
Сергій <61122611+CrazyTapok-bit@users.noreply.github.com>
2024-01-03 17:51:44 +0200

    Connect analytics only in production

---
+++ src/.vuepress/head.js
@@ -0,0 +1,20 @@
+const head = []
+
+const PRODUCTION = process.env.PRODUCTION
+const GTAG = process.env.GTAG
+
+const IS_GTAG = PRODUCTION || GTAG
+
+// Google tag (gtag.js)
+if (IS_GTAG) head.push([
+  "script", { async: true, src: `https://www.googletagmanager.com/gtag/js?id=${GTAG}` }
+], [
+  "script", {},
+  `window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', '${GTAG}');`
+])
+// End Google tag (gtag.js)
+
+export default head
