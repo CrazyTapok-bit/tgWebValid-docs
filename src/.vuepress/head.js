9b0fbff9c7b589109dbbaf96de0dcadbfca224ba
Сергій <61122611+CrazyTapok-bit@users.noreply.github.com>
2024-06-17 08:29:48 +0300

    Connect Monetag

--- src/.vuepress/head.js
+++ src/.vuepress/head.js
@@ -3,9 +3,12 @@ const head = []
 const PRODUCTION = true ?? process.env.PRODUCTION
 const GTAG = process.env.GTAG
 const ADSENSE = process.env.ADSENSE
+const MONETAG = "6f9bca78ef751283975bfdda408ebda7"
+const MONETAG_ZONE = 73276
 
 const IS_GTAG = PRODUCTION && GTAG
 const IS_ADSENSE = PRODUCTION && ADSENSE
+const IS_MONETAG = PRODUCTION && MONETAG
 
 // Google tag (gtag.js)
 if (IS_GTAG) head.push([
@@ -31,5 +34,23 @@ if (IS_ADSENSE) head.push([
   }
 ])
 // End  Google AdSense
+
+// Monetag
+if (IS_MONETAG) head.push([
+  "meta", {
+    name: "monetag",
+    content: MONETAG
+  }
+])
+
+if (IS_MONETAG_ZONE) head.push([
+  "script", {
+    src: "https://alwingulla.com/88/tag.min.js",
+    "data-zone": MONETAG_ZONE,
+    "data-cfasync": "false",
+    async: true
+  }
+])
+// End Monetag
 
 export default head
